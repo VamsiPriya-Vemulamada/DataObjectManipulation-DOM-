979dae8454dca75a6c02e4a11825596bfc13534b
VamsiPriya-Vemulamada <Vemulamadavamsipriya@gmail.com>
2024-08-16 11:20:05 -0400

    pushing new commit

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-var menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
+// var menuLinks = [
+//     { text: 'about', href: '/about' },
+//     { text: 'catalog', href: '/catalog' },
+//     { text: 'orders', href: '/orders' },
+//     { text: 'account', href: '/account' },
+//   ];
+
+  var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
   ];
-
-
 
 let mainEl = document.querySelector("main");
 mainEl.style.backgroundColor = 'var(--main-bg)';
@@ -24,4 +40,35 @@ menuLinks.forEach( link => {
     linkelement.href = link.href;
     topmenuEl.appendChild(linkelement);
 });
+
+const subMenuEl = document.getElementById("sub-menu");
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = '--sub-menu-bg';
+subMenuEl.classList.add( 'flex-around');
+subMenuEl.style.position = "absolute" ;
+subMenuEl.style.top = 0;
+
+// Add Event Listener
+const topMenuLinks = topmenuEl.getElementsByTagName("a");
+topmenuEl.addEventListener('click',function(e){
+  e.preventDefault();
+  if(e.target.targetName !== "A")
+    return;
+  
+console.log(click.target.textContent);
+});
+
+ if(!e.target.classList.contains(active)){
+  const linkObject = menuLinks.find(link => link.text === e.target.target.textContent);
+ 
+ if(linkObject && linkObject.subLinks){
+ subMenuEl.style.top = "100%";
+ }
+ else{
+  subMenuEl.style.top = "0";
+ }
+};
+
+
+
 
